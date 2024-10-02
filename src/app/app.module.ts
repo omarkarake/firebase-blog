030c3f1e19e595445c498918ec2439b7e5eca6e5
omar karake <omar.karake@amalitechtraining.org>
2024-10-02 11:38:03 +0200

    configuring the firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,41 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { NgModule, importProvidersFrom } from '@angular/core';
+import {
+  BrowserModule,
+  provideClientHydration,
+} from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from './modules/shared/shared.module';
 
+import { provideZoneChangeDetection } from '@angular/core';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { firebaseConfig } from './constants/constants';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    RouterModule,
-    SharedModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, RouterModule, SharedModule],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    // provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    importProvidersFrom([
+      AngularFireModule.initializeApp(firebaseConfig),
+      AngularFireAuthModule,
+      AngularFireDatabaseModule,
+      AngularFirestoreModule,
+    ]),
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/constants/constants.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB909F-YqkvsXl_EXd34SKPyIDB6X9Z8y8',
+  authDomain: 'fir-crud-app-cac59.firebaseapp.com',
+  databaseURL: 'https://fir-crud-app-cac59-default-rtdb.firebaseio.com',
+  projectId: 'fir-crud-app-cac59',
+  storageBucket: 'fir-crud-app-cac59.appspot.com',
+  messagingSenderId: '225561738484',
+  appId: '1:225561738484:web:8f92a3179e52db471332a1',
+  measurementId: 'G-MQ9NP8GS5K',
+};
