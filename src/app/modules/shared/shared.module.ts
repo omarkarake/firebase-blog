579a2c903193f05e5df1c2a7dfb98cfc14ca6dc4
omar karake <omar.karake@amalitechtraining.org>
2024-10-03 16:50:24 +0200

    ft: implementing the hero-content with cards

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../components/button/button.component';
 import { InputLabelComponent } from '../../components/input-label/input-label.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { CardComponent } from '../../components/card/card.component';
 
 @NgModule({
-  declarations: [ButtonComponent, InputLabelComponent],
+  declarations: [ButtonComponent, InputLabelComponent, CardComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [ButtonComponent, InputLabelComponent, ReactiveFormsModule], // Exporting ButtonComponent to be used in other modules
+  exports: [ButtonComponent, InputLabelComponent, ReactiveFormsModule, CardComponent], // Exporting ButtonComponent to be used in other modules
 })
 export class SharedModule {}
