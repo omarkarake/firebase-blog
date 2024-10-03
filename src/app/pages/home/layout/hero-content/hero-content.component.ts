8d8240dcdd2222d3fe73ff711892a6170583a4ff
omar karake <omar.karake@amalitechtraining.org>
2024-10-03 19:55:07 +0200

    feat(home): add smooth scroll to app-card section on 'Read Latest Blogs' button click
    - Implemented smooth scrolling behavior using the scrollIntoView() method when the 'Read Latest Blogs' button is clicked.
    - Used Angular's ViewChild to reference the pp-card section and trigger the scroll programmatically.
    - Updated the template to include a #cardSection reference for smooth scrolling functionality.

--- src/app/pages/home/layout/hero-content/hero-content.component.ts
+++ src/app/pages/home/layout/hero-content/hero-content.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-hero-content',
   templateUrl: './hero-content.component.html',
-  styleUrl: './hero-content.component.css'
+  styleUrl: './hero-content.component.css',
 })
 export class HeroContentComponent {
+  // Use ViewChild to access the card section
+  @ViewChild('cardSection') cardSection!: ElementRef;
 
+  // Scroll to the card section
+  scrollToCards() {
+    this.cardSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
 }
