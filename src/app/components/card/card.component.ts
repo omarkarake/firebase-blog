02e79ad41d0c1475e1392d1f7a4f25a73a89c02a
omar karake <omar.karake@amalitechtraining.org>
2024-10-04 12:40:51 +0200

    ft: getting the blog data and render it at the hero-content page

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
-
+  @Input() image: string = 'Image preview';
+  @Input() title: string = 'Title preview';
+  @Input() description: string = 'Description preview';
+  @Input() id: string = 'ID preview';
 }
--- src/app/pages/home/layout/hero-content/hero-content.component.ts
+++ src/app/pages/home/layout/hero-content/hero-content.component.ts
@@ -1,16 +1,37 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { BlogfireService } from '../../../../services/blogfire/blogfire.service';
+import { Blog } from '../../../../models/blog.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-hero-content',
   templateUrl: './hero-content.component.html',
   styleUrl: './hero-content.component.css',
 })
-export class HeroContentComponent {
+export class HeroContentComponent implements OnInit, OnDestroy {
+  blogs$: Observable<Blog[]> | undefined;
+
   // Use ViewChild to access the card section
   @ViewChild('cardSection') cardSection!: ElementRef;
+
+  constructor(
+    private blogfireService: BlogfireService
+  ) {}
 
   // Scroll to the card section
   scrollToCards() {
     this.cardSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  ngOnInit(): void {
+    this.blogs$ = this.blogfireService.getBlogsCollection();
+
+    this.blogfireService.getCommentsCollection().subscribe((comments) => {
+      console.log(comments);
+    });
+  }
+
+  ngOnDestroy(): void {
+    // Clean
   }
 }
